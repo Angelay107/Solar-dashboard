de8c96c8105118c5d1e61cd7240013c8167b37ff
Angelay107 <47455540+Angelay107@users.noreply.github.com>
2025-05-02 23:34:00 -0400

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -4,7 +4,7 @@ Created on Wed Apr 30 20:00:03 2025
 
 @author: alpha
 """
-import numpy as np
+import numpy-financial as npf
 
 def solar_cash_flows(system_size_kw, electricity_price):
     install_cost = 2.50 * 1000 * system_size_kw
@@ -25,8 +25,8 @@ def solar_cash_flows(system_size_kw, electricity_price):
             electricity_price *= 1.025
         cash_flows.append(cash)
 
-    irr = np.irr(cash_flows)
-    cum_cash = np.cumsum(cash_flows)
+    irr = npf.irr(cash_flows)
+    cum_cash = npf.cumsum(cash_flows)
     payback_year = next((i for i, x in enumerate(cum_cash) if x > 0), None)
     
     return cash_flows, irr, payback_year, install_cost, annual_gen_kwh
